0329e81cd070e4710c5c297cd5656989023f077c
KatarinaBenovska <kat.benovska@gmail.com>
2023-09-29 01:23:03 +0200

    created input form

--- Z1_forms/Z1_forms/Form1.cs
+++ Z1_forms/Z1_forms/Form1.cs
@@ -13,6 +13,7 @@ namespace Z1_forms
 {
     public partial class Form1 : Form
     {
+        
         public Form1()
         {
             InitializeComponent();
@@ -23,26 +24,78 @@ namespace Z1_forms
             MessageBox.Show("Transform Clicked!");
         }
 
-        private void checkBtn_Click(object sender, EventArgs e)
+       /* private void checkBtn_Click(object sender, EventArgs e)
         {
-            MessageBox.Show(textBox1.Text);
-        }
+            MessageBox.Show(textBox.Text);
+        }*/
 
         private void saveBtn_Click(object sender, EventArgs e)
         {
             POC_model data = new POC_model();
-            data.textInput = textBox1.Text;
-            data.boolInput = checkBox1.Checked;
-            data.numInput = numericUpDown1.Value;
+            //data.textInput = textBox1.Text;
+            data.boolInput = checkBox.Checked;
+            //data.numInput = numericUpDown1.Value;
             //data.boolInput = textBox3.Text;
 
 
             FormManager.SaveData(data);
         }
 
-        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        private void checkBox_CheckedChanged(object sender, EventArgs e)
         {
 
+        }
+        
+        private void numericUpDownNumberOfChildren_ValueChanged(Object sender, EventArgs e) {
+
+            int countAfterClick =Convert.ToInt32(Math.Round(numericUpDownNumberOfChildren.Value, 0));
+            int countBeforeClick = int.Parse(numericUpDownNumberOfChildren.Text);
+            if (countAfterClick == 6 && countBeforeClick == 5)
+            {
+                Controls.Add(labelChildName2);
+                Controls.Add(labelChildAge2);
+            }
+            if (countAfterClick == 5 && countBeforeClick == 6)
+            {
+                Controls.Remove(labelChildName2);
+                Controls.Remove(labelChildAge2);
+            }
+            if (countBeforeClick < countAfterClick)
+            {
+                //Create the dynamic TextBox.
+                TextBox textboxChildName = new TextBox();
+                textboxChildName.Size = new Size(153, 22);
+                textboxChildName.Name = "textboxChildName_" + (countAfterClick);
+                textboxChildName.TabIndex = 18+2*countAfterClick-3;
+                
+                NumericUpDown numericUpDownChildAge = new NumericUpDown();
+                numericUpDownChildAge.Name = "numericUpDownChildAge_" + (countAfterClick);
+                numericUpDownChildAge.Size = new Size(90, 22);
+                numericUpDownChildAge.Controls[0].Visible = false;
+                numericUpDownChildAge.Minimum = 0;
+                numericUpDownChildAge.Maximum = 26;
+                numericUpDownChildAge.DecimalPlaces = 0;
+                numericUpDownChildAge.TabIndex = 18+2*countAfterClick-2;
+                if (countAfterClick <= 5)
+                {
+                    textboxChildName.Location = new Point(8, 472 + (22 * (countAfterClick - 1)));
+                    numericUpDownChildAge.Location = new Point(165, 472 + (22 * (countAfterClick - 1)));
+                }
+                else
+                {
+                    textboxChildName.Location = new Point(280, 472 + (22 * (countAfterClick - 6)));
+                    numericUpDownChildAge.Location = new Point(435, 472 + (22 * (countAfterClick - 6)));
+                }
+                Controls.Add(textboxChildName);
+                Controls.Add(numericUpDownChildAge);
+            }
+            else if(countBeforeClick > countAfterClick)
+            {
+                Controls.Remove(Controls.Find("textboxChildName_" + countBeforeClick, true)[0]);
+                Controls.Remove(Controls.Find("numericUpDownChildAge_" + countBeforeClick, true)[0]);
+            }
+            
+            
         }
     }
 }
